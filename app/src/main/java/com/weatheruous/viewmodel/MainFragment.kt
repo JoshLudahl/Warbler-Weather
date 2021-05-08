ee97d5b4ff15878cd32da218f08b19189313c4d5
Joshy <josh.ludahl@gmail.com>
2021-05-07 21:49:50 -0700

    Updated dependencies.

--- app/src/main/java/com/weatheruous/viewmodel/MainFragment.kt
+++ app/src/main/java/com/weatheruous/viewmodel/MainFragment.kt
@@ -4,16 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
-import androidx.databinding.ViewDataBinding
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
-import com.weatheruous.R
 import com.weatheruous.data.WeatherDatabase
 import com.weatheruous.databinding.FragmentMainBinding
-import kotlinx.android.synthetic.main.activity_main.*
-
 
 /**
  * A simple [Fragment] subclass.
