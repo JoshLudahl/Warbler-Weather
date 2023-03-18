1214249b05ad71fabae3e3b2999833d8a5bf385e
joshy <josh.ludahl@gmail.com>
2023-03-17 17:54:49 -0700

    Formatting

--- app/src/main/java/com/weatheruous/data/WeatherUrls.kt
+++ app/src/main/java/com/weatheruous/data/WeatherUrls.kt
@@ -6,17 +6,12 @@ import androidx.room.PrimaryKey
 
 @Entity(tableName = "location_information")
 data class WeatherUrls(
-
     @PrimaryKey(autoGenerate = true)
     var id: Long = 0L,
-
     @ColumnInfo(name = "grid_points")
     var gridpoints: String = "107,99",
-
     @ColumnInfo(name = "system_of_measurement")
     var scale: String = "imperial",
-
     @ColumnInfo(name = "lat_long")
     var location: String = "45.45945945945946,-122.78933059959456"
-
 )
