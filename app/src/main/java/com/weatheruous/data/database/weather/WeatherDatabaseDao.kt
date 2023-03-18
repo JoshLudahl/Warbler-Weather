694e50049110d94922a81de331fa34625782dffc
joshy <josh.ludahl@gmail.com>
2023-03-17 17:26:31 -0700

    Reset database reset value

--- app/src/main/java/com/weatheruous/data/database/weather/WeatherDatabaseDao.kt
+++ app/src/main/java/com/weatheruous/data/database/weather/WeatherDatabaseDao.kt
@@ -1,10 +1,6 @@
 package com.weatheruous.data.database.weather
 
 import androidx.room.Dao
-import androidx.room.Insert
-import com.weatheruous.data.model.WeatherData
 
 @Dao
-interface WeatherDatabaseDao {
-
-}
+interface WeatherDatabaseDao
