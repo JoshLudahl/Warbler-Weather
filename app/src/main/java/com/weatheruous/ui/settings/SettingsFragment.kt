71e6e3b901126f67bf5d60a37d1bff027c17d268
joshy <josh.ludahl@gmail.com>
2023-03-21 17:12:45 -0700

    Added DI to settings screen

--- app/src/main/java/com/weatheruous/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/weatheruous/ui/settings/SettingsFragment.kt
@@ -3,7 +3,9 @@ package com.weatheruous.ui.settings
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.weatheruous.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class SettingsFragment : Fragment(R.layout.fragment_settings) {
 
     val viewModel: SettingsViewModel by viewModels()
