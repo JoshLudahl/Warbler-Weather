af3a7945cee5df5d9e29c1c6f93ce72ce7d67b7a
Josh Ludahl <josh.ludahl@gmail.com>
2023-06-22 10:43:12 -0700

    Fixed crash, leaks, updated formatting.

--- app/src/main/java/com/warbler/ui/forecast/ForecastFragment.kt
+++ app/src/main/java/com/warbler/ui/forecast/ForecastFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.Navigation.findNavController
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.GridLayoutManager
@@ -166,8 +167,8 @@ class ForecastFragment : Fragment(R.layout.fragment_forecast) {
     }
 
     private fun setupListeners() {
-        binding.backIcon.setOnClickListener { view ->
-            view.findNavController().navigateUp()
+        binding.backIcon.setOnClickListener {
+            findNavController(it).navigateUp()
         }
     }
 
