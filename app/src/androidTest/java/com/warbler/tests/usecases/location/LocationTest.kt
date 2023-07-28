5af2705d825ad01c3857cf72f797818e850c64e4
Josh Ludahl <josh.ludahl@gmail.com>
2023-07-28 15:35:10 -0700

    Version Bump

---
+++ app/src/androidTest/java/com/warbler/tests/usecases/location/LocationTest.kt
@@ -0,0 +1,12 @@
+package com.warbler.tests.usecases.location
+
+import com.warbler.tests.BaseTest
+import org.junit.Test
+
+class LocationTest: BaseTest() {
+
+    @Test
+    fun checkLocation() {
+
+    }
+}
---
+++ app/src/androidTest/java/com/warbler/tests/usecases/main/MainScreenTest.kt
@@ -0,0 +1,12 @@
+package com.warbler.tests.usecases.main
+
+import com.warbler.tests.BaseTest
+import org.junit.Test
+
+class MainScreenTest: BaseTest() {
+
+    @Test
+    fun checkMain() {
+
+    }
+}
