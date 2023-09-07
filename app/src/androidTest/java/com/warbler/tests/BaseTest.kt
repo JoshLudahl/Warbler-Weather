2cd896f8c42fe200e4b44c176ed2e2548c6e03c4
Josh Ludahl <josh.ludahl@gmail.com>
2023-09-07 16:39:23 -0700

    Updated dependencies

--- app/src/androidTest/java/com/warbler/tests/BaseTest.kt
+++ app/src/androidTest/java/com/warbler/tests/BaseTest.kt
@@ -1,6 +1,7 @@
 package com.warbler.tests
 
 import androidx.test.ext.junit.rules.ActivityScenarioRule
+import com.softklass.elk.rules.EspressoSetupRule
 import com.warbler.MainActivity
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
@@ -13,10 +14,12 @@ abstract class BaseTest {
 
     private val hiltAndroidRule = HiltAndroidRule(this)
     private val activityScenarioRule = ActivityScenarioRule(MainActivity::class.java)
+    private val espressoRule = EspressoSetupRule(activityScenarioRule)
 
     @get:Rule
     val ruleChain: RuleChain = RuleChain
         .outerRule(hiltAndroidRule)
+        .around(espressoRule)
         .around(activityScenarioRule)
 
     @Before
