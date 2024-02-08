5ac9ed32b5f7ca864324ce1b2a55465e5f7d5bec
Josh Ludahl <josh.ludahl@gmail.com>
2024-02-08 12:32:58 -0800

    feature: added share

--- app/src/main/java/com/warbler/ui/forecast/viewpager/ViewPagerAdapter.kt
+++ app/src/main/java/com/warbler/ui/forecast/viewpager/ViewPagerAdapter.kt
@@ -1,7 +1,13 @@
 package com.warbler.ui.forecast.viewpager
 
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.Canvas
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat.startActivity
+import androidx.core.content.FileProvider
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.warbler.data.model.weather.Conversion
@@ -10,6 +16,9 @@ import com.warbler.data.model.weather.Forecast
 import com.warbler.data.model.weather.WeatherDataSourceDto
 import com.warbler.databinding.ForecastViewPagerItemBinding
 import com.warbler.ui.MainWeatherDetailItemAdapter
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
 import kotlin.math.roundToInt
 
 class ViewPagerAdapter(
@@ -44,6 +53,48 @@ class ViewPagerAdapter(
             itemBinding.weatherDetailRecyclerView.adapter = weatherDetailItemAdapter
             itemBinding.weatherDetailRecyclerView.layoutManager = layoutManager
             weatherDetailItemAdapter.setItems(weatherDetailList)
+
+            itemBinding.shareIcon.setOnClickListener {
+                val sharableView = itemBinding.headingConstraint
+                val context = sharableView.context
+
+                itemBinding.shareIcon.visibility = View.GONE
+
+                val bitmap = Bitmap.createBitmap(sharableView.width, sharableView.height, Bitmap.Config.ARGB_8888)
+                val canvas = Canvas(bitmap)
+                sharableView.draw(canvas)
+
+                itemBinding.shareIcon.visibility = View.VISIBLE
+                // save bitmap to cache directory
+                // save bitmap to cache directory
+                try {
+                    val cachePath = File(context.cacheDir, "images")
+                    cachePath.mkdirs() // don't forget to make the directory
+                    val stream: FileOutputStream =
+                        FileOutputStream("$cachePath/image.png") // overwrites this image every time
+                    bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream)
+                    stream.close()
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                }
+
+                val imagePath = File(context.cacheDir, "images")
+                val newFile = File(imagePath, "image.png")
+                val contentUri =
+                    FileProvider.getUriForFile(context, "com.warbler.fileprovider", newFile)
+
+                val sendIntent: Intent =
+                    Intent().apply {
+                        action = Intent.ACTION_SEND
+                        addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+                        setDataAndType(contentUri, "image/*")
+                        putExtra(Intent.EXTRA_STREAM, contentUri)
+                    }
+
+                val shareIntent = Intent.createChooser(sendIntent, null)
+                startActivity(context, shareIntent, null)
+            }
+
             itemBinding.executePendingBindings()
         }
     }
