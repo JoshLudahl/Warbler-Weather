4b5889588088f2f1f7a6e4b4bd757f05403ec2cd
joshy <josh.ludahl@gmail.com>
2024-05-18 18:11:48 -0700

    Updated styling.

--- app/src/main/java/com/warbler/ui/MainBindingAdapters.kt
+++ app/src/main/java/com/warbler/ui/MainBindingAdapters.kt
@@ -1,6 +1,8 @@
 package com.warbler.ui
 
+import android.content.res.ColorStateList
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import com.warbler.R
 
@@ -15,5 +17,19 @@ fun ImageView.setTints(index: Int) {
         else -> R.color.AQI_1
     }.let {
         setColorFilter(resources.getColor(it, context.theme))
+    }
+}
+
+@BindingAdapter("aqiColorTextBackground")
+fun TextView.setTinty(index: Int) {
+    when (index) {
+        1 -> R.color.AQI_1
+        2 -> R.color.AQI_2
+        3 -> R.color.AQI_3
+        4 -> R.color.AQI_4
+        5 -> R.color.AQI_5
+        else -> R.color.AQI_1
+    }.let {
+        backgroundTintList = ColorStateList.valueOf(resources.getColor(it, context.theme))
     }
 }
