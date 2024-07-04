2b764cf61dabfb1cf3b1269e218ffd852c6942a2
joshy <josh.ludahl@gmail.com>
2024-07-03 19:42:42 -0700

    chore: cleaned up unused import

--- app/src/main/java/com/warbler/ui/MainWeatherFragment.kt
+++ app/src/main/java/com/warbler/ui/MainWeatherFragment.kt
@@ -2,7 +2,6 @@ package com.warbler.ui
 
 import android.graphics.Color
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
