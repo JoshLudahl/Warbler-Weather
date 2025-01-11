a58221f8792b6e40284dc2de81e267e0e1d38684
joshy <josh.ludahl@gmail.com>
2025-01-10 16:36:40 -0800

    Updated Gradle to use Kotlin, moving from Groovy, at the project level.

---
+++ build.gradle.kts
@@ -0,0 +1,23 @@
+
+buildscript {
+    dependencies {
+        classpath(libs.gradle)
+        classpath(libs.hilt.android.gradle.plugin)
+        classpath(libs.kotlin.gradle.plugin)
+        classpath(libs.navigation.safe.args.gradle.plugin)
+        classpath(libs.google.services)
+        classpath(libs.firebase.crashlytics.gradle)
+        classpath(libs.perf.plugin)
+    }
+}
+
+plugins {
+    id("org.jlleitschuh.gradle.ktlint") version "12.1.1"
+    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.0.21"
+    id("com.google.devtools.ksp") version "2.1.0-1.0.29" apply false
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.layout.buildDirectory.get())
+}
