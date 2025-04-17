1f51d5ab0f6cce8ecf6767a43f207fc8bc16f8b0
joshy <josh.ludahl@gmail.com>
2025-04-16 20:59:45 -0700

    chore: Update versionCode to 84

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,7 +23,7 @@ android {
         applicationId = "com.softklass.warbler"
         minSdk = 26
         targetSdk = target
-        versionCode = 83
+        versionCode = 84
         versionName = "0.$versionCode"
         testInstrumentationRunner = "com.warbler.config.HiltAndroidJUnitRunner"
         testInstrumentationRunnerArguments.putAll(mutableMapOf("clearPackageData" to "true"))
