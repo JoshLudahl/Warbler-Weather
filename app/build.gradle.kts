a49a49043f417a5de5b0fa9b10f6803090219937
joshy <josh.ludahl@gmail.com>
2025-08-17 17:13:20 -0700

    Increment versionCode to 89
    The `versionCode` in `app/build.gradle.kts` has been updated from 88 to 89. The `versionName` is now "0.89".

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,7 +23,7 @@ android {
         applicationId = "com.softklass.warbler"
         minSdk = 26
         targetSdk = target
-        versionCode = 88
+        versionCode = 89
         versionName = "0.$versionCode"
         testInstrumentationRunner = "com.warbler.config.HiltAndroidJUnitRunner"
         testInstrumentationRunnerArguments.putAll(mutableMapOf("clearPackageData" to "true"))
