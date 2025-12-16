2a13d6c0eebe2e1eefe3e2e5a47f5d63fb2116a5
joshy <josh.ludahl@gmail.com>
2025-12-16 11:15:05 -0800

    feat: Increment versionCode to 90
    This commit increments the `versionCode` in `app/build.gradle.kts` from 89 to 90. The `versionName` is derived from this `versionCode`.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,7 +23,7 @@ android {
         applicationId = "com.softklass.warbler"
         minSdk = 26
         targetSdk = target
-        versionCode = 89
+        versionCode = 90
         versionName = "0.$versionCode"
         testInstrumentationRunner = "com.warbler.config.HiltAndroidJUnitRunner"
         testInstrumentationRunnerArguments.putAll(mutableMapOf("clearPackageData" to "true"))
