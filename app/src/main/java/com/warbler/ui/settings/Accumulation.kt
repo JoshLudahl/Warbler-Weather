05826fb140ec0a60e1eb7dad29ed403efc3c065d
joshy <josh.ludahl@gmail.com>
2026-08-03 08:24:00 -0700

    auto-agent: update composable

--- app/src/main/java/com/warbler/ui/settings/Accumulation.kt
+++ app/src/main/java/com/warbler/ui/settings/Accumulation.kt
@@ -1,11 +1,6 @@
 package com.warbler.ui.settings
 
-import androidx.annotation.IdRes
-import com.warbler.R
-
-enum class Accumulation(
-    @IdRes val id: Int,
-) {
-    INCHES_PER_HOUR(R.id.radio_inph),
-    MILLIMETERS_PER_HOUR(R.id.radio_mmph),
+enum class Accumulation {
+    INCHES_PER_HOUR,
+    MILLIMETERS_PER_HOUR,
 }
--- app/src/main/java/com/warbler/ui/settings/Speed.kt
+++ app/src/main/java/com/warbler/ui/settings/Speed.kt
@@ -1,12 +1,7 @@
 package com.warbler.ui.settings
 
-import androidx.annotation.IdRes
-import com.warbler.R
-
-enum class Speed(
-    @IdRes val id: Int,
-) {
-    MPH(R.id.radio_mph),
-    KPH(R.id.radio_kph),
-    MPS(R.id.radio_mps),
+enum class Speed {
+    MPH,
+    KPH,
+    MPS,
 }
