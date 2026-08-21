21261cff6013e0bf312af57639a11e027ac0bd56
joshy <josh.ludahl@gmail.com>
2026-08-20 17:41:06 -0700

    auto-agent: update cards

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ configure<ApplicationExtension> {
         applicationId = "com.softklass.warbler"
         minSdk = 26
         targetSdk = target
-        versionCode = 99
-        versionName = "2026.08.18"
+        versionCode = 100
+        versionName = "2026.08.20"
         testInstrumentationRunner = "com.warbler.config.HiltAndroidJUnitRunner"
         testInstrumentationRunnerArguments.putAll(mutableMapOf("clearPackageData" to "true"))
         vectorDrawables.useSupportLibrary = true
